d9cd378432970db88d9d629408a6f46a5cf128a1
Yaml <993620479@qq.com>
2023-06-12 20:40:44 +0800

    P45,网关搭建完毕

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,11 @@
 package com.atguigu.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -25,5 +29,50 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
         return new PageUtils(page);
     }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1.查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        //2.组装出父子的结构
+        //2.1.找出所有的一级分类
+        List<CategoryEntity> level1Menus = entities.stream()
+                .filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                .map(menu -> {
+                    menu.setChildren(getChildrens(menu, entities));
+                    return menu;
+                })
+                .sorted((menu1, menu2) ->
+                    (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort())
+                )
+                .collect(Collectors.toList());
+
+        return level1Menus;
+    }
+
+    /**
+     * 递归查找所有菜单的子菜单
+     * @param root 当前菜单
+     * @param all 所有菜单
+     * @return
+     */
+    private List<CategoryEntity> getChildrens(CategoryEntity root, List<CategoryEntity> all){
+        List<CategoryEntity> children = all.stream()
+                //找到当前菜单的子菜单
+                .filter(categoryEntity -> categoryEntity.getParentCid().equals(root.getCatId()))
+                //每一个子菜单可能还有子菜单，所以递归进行查找子菜单的子菜单并排序
+                .map(categoryEntity -> {
+                    categoryEntity.setChildren(getChildrens(categoryEntity, all));
+                    return categoryEntity;
+                })
+                .sorted((menu1, menu2) ->
+                    (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort())
+                )
+                .collect(Collectors.toList());
+
+        return children;
+    }
+
 
 }
